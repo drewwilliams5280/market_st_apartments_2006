114ae01c9439663017a4e46d2a081a3ae5da62a5
Drew Williams <drewwilliams5280@gmail.com>
2020-07-14 09:10:19 -0600

    Adds Renter class and tests it exists

---
+++ test/renter_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def test_it_exists
+    renter = Renter.new("Jessie")
+    assert_instance_of Renter, renter 
+  end
+
+end
