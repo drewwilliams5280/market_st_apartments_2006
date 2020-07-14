5cadbff588585f7c4b0bbab51743ce29705b1f97
Drew Williams <drewwilliams5280@gmail.com>
2020-07-14 09:11:28 -0600

    Tests it has name

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -6,7 +6,12 @@ class RenterTest < Minitest::Test
 
   def test_it_exists
     renter = Renter.new("Jessie")
-    assert_instance_of Renter, renter 
+    assert_instance_of Renter, renter
+  end
+
+  def test_it_has_a_name
+    renter = Renter.new("Jessie")
+    assert_equal "Jessie", renter.name 
   end
 
 end
