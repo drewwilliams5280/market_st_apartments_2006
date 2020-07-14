04533cbfde8bf5badf8922b0f47a7d3d68cafb78
Drew Williams <drewwilliams5280@gmail.com>
2020-07-14 09:42:49 -0600

    Tests apartments initialize

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -11,5 +11,13 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, unit1
   end
 
+  def test_it_initializes
+    unit1 = Apartment.new("A1", 1200, 1, 1)
+
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
 
 end
